2586a24bef25e2abc0313e7c7c3bfcf0a0c34782
Raymond Barley <raymondbarley@payasyougobackoffice.com>
2019-12-15 09:09:25 -0500

    Added job name to jobs not scheduled query.

--- SQL AGENT JOB ACTIVITY QUERIES.sql
+++ SQL AGENT JOB ACTIVITY QUERIES.sql
@@ -5,6 +5,8 @@ FROM msdb.dbo.sysjobactivity;
 
 -- get jobs that are not scheduled
 SELECT
-	*
-FROM msdb.dbo.sysjobactivity
+	j.[name]
+,	a.*
+FROM msdb.dbo.sysjobactivity a
+JOIN msdb.dbo.sysjobs j ON j.job_id = a.job_id
 WHERE next_scheduled_run_date IS NULL;
